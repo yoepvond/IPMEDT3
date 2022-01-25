93eea0b8750dccc0eecb4870efdffb82bc9015d3
Danielsneekes20 <danielsneekes@hotmail.com>
2022-01-25 12:42:33 +0100

    update

--- main.js
+++ main.js
@@ -23,5 +23,17 @@ for(let i = 0; i < play.length; i++){
     play[i].onmouseleave = (event) => {
         play[i].setAttribute("color", "grey");
     }
+    play[i].onclick = (event) => {
+        play[i].setAttribute("color", "yellow");
+    }
 }
 
+// const timer = ms => new Promise(res => setTimeout(res, ms));
+// async function start(){
+//     for (let i = 0; i < 10; i++) {
+//         await timer(1500);
+//         body[0].setAttribute("scale",{x:0.1,y:0.1,z:0.1});
+//         await timer(1500);
+//         body[0].setAttribute("scale",{x:0.10,y:0.10,z:0.11});
+//     }
+// }
