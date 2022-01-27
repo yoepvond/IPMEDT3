bb99ce6f90615224a86d2cfc8553ab1c31fd784b
yoepvond <47593477+yoepvond@users.noreply.github.com>
2022-01-27 14:08:32 +0100

    Stappen en tas

--- js/engecomponent.js
+++ js/engecomponent.js
@@ -6,16 +6,38 @@ AFRAME.registerComponent('compressie', {
     init: function() {
 
         const timer = ms => new Promise(res => setTimeout(res, ms));
+        let starter = document.getElementsByClassName("starter")
+        let shocker = document.getElementsByClassName("aed")
+        let drukker = document.getElementsByClassName("drukken")
+        let info = document.getElementById("startText")
+        let adem = document.getElementById("adem")
 
         starter[0].onclick = async(event) => {
             if (stap == 1) {
+                info.setAttribute("Value", "Check ademhaling")
                 tweevingers.setAttribute("visible", true);
                 playerHand.setAttribute("visible", false);
                 await timer(3000);
                 tweevingers.setAttribute("visible", false);
+                playerHand.setAttribute("visible", true);
                 stap++
+                shocker[0].setAttribute("visible", true);
             }
+        }
+        shocker[0].onclick = async(event) => {
             if (stap == 2) {
+                aed.setAttribute("visible", true);
+                console.log("patient");
+                await (shock());
+                aed.setAttribute("visible", false)
+                console.log(stap)
+                stap++
+                drukker[0].setAttribute("visible", true);
+                shocker[0].setAttribute("visible", false);
+            }
+        }
+        drukker[0].onclick = async(event) => {
+            if (stap == 3) {
                 el = handen;
                 el.setAttribute("visible", true);
                 playerHand.setAttribute("visible", false);
@@ -24,8 +46,21 @@ AFRAME.registerComponent('compressie', {
                 playerHand.setAttribute("visible", true);
                 el.setAttribute("visible", false)
                 console.log(stap)
+                let worked = Math.floor(Math.random() * 1)
+                if (worked == 0) {
+                    console.log("0")
+                        .setAttribute("color", "red");
+                    drukker[0].setAttribute("visible", false)
+                } else {
+                    console.log("1")
+                    drukker[0].setAttribute("visible", true);
+                    ele.setAttribute("color", "blue");
+
+                }
             }
         }
+
+
         async function start() {
             el = handen;
             ele = starter[0]
@@ -43,10 +78,21 @@ AFRAME.registerComponent('compressie', {
             if (worked == 1) {
                 console.log("1")
                 ele.setAttribute("color", "red");
+                drukker[0].setAttribute("visible", false)
             } else {
                 console.log("0")
+                drukker[0].setAttribute("visible", true);
                 ele.setAttribute("color", "blue");
+
             }
+        }
+        async function shock() {
+            await timer(1000);
+            patient.setAttribute("scale", { x: 1.01, y: 1.01, z: 1.01 });
+            await timer(300);
+            patient.setAttribute("scale", { x: 1, y: 1, z: 1 });
+            await timer(1000);
+
         }
     }
 });
--- js/hoofdkleur.js
+++ js/hoofdkleur.js
@@ -1,10 +1,16 @@
 const object = document.getElementById("object");
 const vloer = document.getElementById("vloer");
 const hoofd = document.getElementById("hoofd")
+const text = document.getElementById("text");
+const plek = document.getElementById("place");
+const tas2 = document.getElementById("object2");
+let timer = document.getElementById("timer");
+
+
 
 
 //hoofdpijn
-/*var timeLeft = 360
+var timeLeft = 360
 var timerId = setInterval(countdown, 1000);
 
 function countdown() {
@@ -12,36 +18,43 @@ function countdown() {
         clearTimeout(timerId)
         console.log("dood");
     } else if (timeLeft == 300) {
-        console.log("kaas");
         hoofd.setAttribute("color", "grey")
         timeLeft--;
     } else if (timeLeft == 240) {
-        console.log("kaas");
+
         hoofd.setAttribute("opacity", "0.2")
         timeLeft--;
     } else if (timeLeft == 180) {
-        console.log("kaas");
+
         hoofd.setAttribute("color", "pink")
 
         timeLeft--;
     } else if (timeLeft == 120) {
-        console.log("kaas");
+
         hoofd.setAttribute("color", "orchid")
         timeLeft--;
     } else if (timeLeft == 60) {
-        console.log("kaas");
+
         hoofd.setAttribute("color", "blue")
         timeLeft--;
     } else {
-        console.log(timeLeft);
+        timer.setAttribute("value", timeLeft)
         timeLeft--;
     }
-}*/
+}
 
 
 //oppakken blokje
 object.onclick = (event) => {
-    object.setAttribute("spring", "target: #camera; damping: 500; stiffness: 500;");
+    object.setAttribute("spring", "target: #camera; damping: 100; stiffness: 100;");
+    text.setAttribute("value", "Plaats de tas op de rode bal")
+    plek.onclick = (event) => {
+        console.log("check")
+        object.setAttribute("visible", "false")
+        console.log(object)
+        object2.setAttribute("visible", "true")
+        text.setAttribute("value", "Start met de ademhaling controleren")
+    }
 }
 
 vloer.onclick = (event) => {
